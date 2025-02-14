cab207c4fc25fc6aba2affbf542b386e799bf69c
Pradnya Shelar <100570045+pradnyaa13@users.noreply.github.com>
2025-02-14 23:02:51 +0530

    Add files via upload

---
+++ PlayerApp/PlayerApp/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    internal class Player
+    {
+        private int id;
+        private string name;
+        private int age;
+
+       
+        public Player(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        
+        public Player(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = 0;
+        }
+
+      
+        public int GetId() { return id; }
+        public string GetName() { return name; }
+        public int GetAge() { return age; }
+
+     
+        public Player WhoIsElder(Player other)
+        {
+            if (this.age >= other.age)
+                return this;
+            else
+                return other;
+        }
+
+   
+        public void Display()
+        {
+            Console.WriteLine($"ID: {id}, Name: {name}, Age: {age}");
+        }
+    }
+}
---
+++ PlayerApp/PlayerApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(1, "Pradnya", 25);
+            Player player2 = new Player(2, "Pranav", 30);
+
+            player1.Display();
+            player2.Display();
+
+            Player elder = player1.WhoIsElder(player2);
+            Console.WriteLine($"Elder Player: {elder.GetName()}");
+        }
+    }
+}
