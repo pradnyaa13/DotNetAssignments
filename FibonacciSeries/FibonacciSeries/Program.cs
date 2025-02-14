8f98c0e3427e8dc9bc939dba6eb0882c3e6c7462
Pradnya Shelar <100570045+pradnyaa13@users.noreply.github.com>
2025-02-14 22:44:51 +0530

    Add files via upload

---
+++ FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int element = 8; 
+            int firstElement = 1, secondElement = 1;
+
+            Console.Write(firstElement + " " + secondElement + " "); 
+
+            for (int i = 3; i <= element; i++)
+            {
+                int next = firstElement + secondElement;
+                Console.Write(next + " ");
+                firstElement = secondElement;
+                secondElement = next;
+            }
+        }
+    }
+}
