57ce73b2de38137890bf1382824ce209cc9711a5
Pradnya Shelar <100570045+pradnyaa13@users.noreply.github.com>
2025-02-14 23:16:09 +0530

    Add files via upload

---
+++ ConstructorOverloading/ConstructorOverloading/Add.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorOverloading
+{
+    internal class Add
+    {
+        int x, y;
+        double f;
+        string s;
+
+        
+        public Add(int a, double b)
+        {
+            x = a;
+            f = b;
+        }
+
+        
+        public Add(int a, string b)
+        {
+            y = a;
+            s = b;
+        }
+        public void show()
+        {
+            Console.WriteLine("1st constructor (int + float): {0} ", (x + f));
+
+        }
+
+     
+        public void show1()
+        {
+            Console.WriteLine("2nd constructor (int + string): {0}", (s + y));
+
+        }
+    }
+}
---
+++ ConstructorOverloading/ConstructorOverloading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add add = new Add(10, 20.2);
+            add.show();
+
+            Add add1 = new Add(10, "Roll No. is ");
+            add1.show1();
+        }
+    }
+}
